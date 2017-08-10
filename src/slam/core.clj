ef925d21034a7e70b778e9590e3cd4240f0473f5
Vlad Ki <proger@hackndev.com>
2017-08-10 20:13:54 +0300

    snap

--- src/slam/core.clj
+++ src/slam/core.clj
@@ -1,7 +1,7 @@
 (ns slam.core
   (:require [anglican.core :as ac :refer [doquery]]
             [anglican.runtime :as ar :refer [uniform-continuous mvn normal]]
-            [anglican.emit :as ae :refer [defquery defm]]
+            [anglican.emit :as ae :refer [defquery defm with-primitive-procedures]]
             [anglican.stat :as stat]
             [clojure.core.matrix :as mat]
             [slam.plot :as plot]))
@@ -72,68 +72,106 @@
                 (* (mat/sin orientation) distance)]]
     (->> world
          (iterate #(move% % motion))
+         (map (fn [world] (when (some? world) {:measurements (sense world)
+                                               :world world
+                                               :control motion})))
          (take-while some?)
-         (take 40)
-         (map (fn [world] {:measurements (sense world)
-                           :world world
-                           :control motion})))))
+         (take 40))))
 
 (defn explore
   "keep moving around until you see all landmarks"
   [{num-landmarks :num-landmarks :as world}]
-  (loop [observations (explore-step world)]
+  (loop [observations (explore-step world)
+         niters 0]
     (let [seen
           (->> observations
-               (map :observations)
+               (map :measurements)
                (map #(map :id %))
                (flatten)
                (set))]
+      (when (zero? (rem niters 20))
+        (println "exploring:" niters "passes"
+                 "seen:" (count seen)
+                 "need:" num-landmarks ))
       (if (= num-landmarks (count seen))
         {:observations observations
          :num-landmarks num-landmarks}
-        (recur (concat
-                observations
-                (explore-step world)))))))
+        (recur (concat observations (explore-step world))
+               (inc niters))))))
 
 
 ;; :observations [{:measurements :control :world}]
 (def !data (atom []))
 
-(let [world (random-world [100 100])
-      {obs :observations
-       :as data} (explore world)]
-  (reset! !data data)
-  (count obs))
+(defn simulate []
+  (let [world (random-world [100 100])
+        {obs :observations
+         :as data} (explore world)]
+    (reset! !data data)
+    (count obs)))
+
+;; (simulate)
 
 ;; random variables: N poses + 10 landmarks
 
 (defn ground-truth [{:keys [observations num-landmarks]}]
   (let [any-world (:world (first observations))
         landmarks (:landmarks any-world)
-        poses (map #(-> %
-                        (:world)
-                        (:robot)) observations)]
+        poses (map #(-> % :world :robot) observations)]
     {:poses poses
      :starting-pose (-> any-world (:robot))
      :landmarks landmarks}))
 
 (ground-truth @!data)
 
+;; @!data
+;;(matrix)
 
-(defquery slam-landmarks [;;{:keys [observations num-landmarks]}
-                          thing
-                          ]
-  (println (keys thing))
-  ;; (let [[max-x max-y] (-> (first observations) (:world) (:bounds))
-  ;;       _meh (println max-y max-x)
-  ;;       landmarks
-  ;;       (repeatedly num-landmarks #({:x (uniform-continuous 0 max-x)
-  ;;                                    :y (uniform-continuous 0 max-y)}))]
-  ;;   landmarks)
+(defm sample-values [map-]
+  (into {} (map (fn [[k v]] [k (sample v)]) map-)))
 
-  42)
+(def magnitude #(mat/magnitude %))
 
+;; given true poses and noisy measurements,
+;; sample from landmark positions, try to compute their measurements
+;; and (observe (normal fake-measurement big-sigma) real-measurement)
 
+;; using magnitude instead of mat/distance because it takes one argument
+(with-primitive-procedures [magnitude]
+  (defquery slam-landmarks [{:keys [observations num-landmarks]}]
+    (let [[max-x max-y] (-> (first observations) (:world) (:bounds))
+          landmark-dists (map (fn [i] [i [(uniform-continuous 0 max-x)
+                                          (uniform-continuous 0 max-y)]])
+                              (range num-landmarks))
+          ;;true-poses (map #(-> % :world :robot) observations)
+          ;;samples (map #(sample-values %) landmarks)
+          ]
+      (reduce
+       (fn [state {:keys [measurements world control]}]
+         (let [[pose-x pose-y] (:robot world)
+               sensor-sigma (:sensor-sigma world)
+               landmark-guesses (map
+                                 (fn [[i [x-dist y-dist]]]
+                                   [i [(sample x-dist) (sample y-dist)]])
+                                 landmark-dists)
+               distance-guesses (map
+                                 (fn [[id [x y]]]
+                                   (println [x y] (mat/magnitude [x y]))
+                                   (mat/magnitude
+                                    [(- pose-x x) (- pose-y y)]))
+                                 landmark-guesses)
+               ;;_ (println distance-guesses)
+               ]
+
+              (reduce
+               (fn [_ {:keys [id dist]}]
+                 (observe (normal (nth distance-guesses id) sensor-sigma) dist))
+               nil
+               measurements)
+
+              landmark-guesses))
+       []
+       observations))))
 
 (take 1 (doquery
          :importance
